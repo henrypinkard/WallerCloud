f66adfbb132c580bca19b9b65e67188dc72b6342
Henry Pinkard <henry.pinkard@gmail.com>
2019-07-09 08:33:57 -0700

    hash function

--- wallercloud.sh
+++ wallercloud.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#alias for linuz systems
+function hash() {
+  if [[ "$OSTYPE" == "linux"* ]]; then
+   sha1sum "$1"
+  else
+   shasum "$1"
+  fi
+}
 
 function uploadPath() {
 
@@ -37,8 +45,7 @@ function uploadPath() {
   LOCAL_PARENT_DIR="$(dirname "$LOCAL_PATH")"/
   LOCAL_RELATIVE_PATH=${LOCAL_PATH#"$LOCAL_PARENT_DIR"}
   #alias this because they have different names on linux and mac
-  alias shasum=sha1sum
-  tar cf - -C "$LOCAL_PARENT_DIR" "$LOCAL_RELATIVE_PATH"  | pv -s $(du -sk "$LOCAL_PATH" | awk '{print $1}')k | pigz -4 - | tee >(shasum > "${LOCAL_SPLIT_DIR}/${BASENAME}_sha1.txt") | split -b 1024m - "${LOCAL_SPLIT_DIR}/${BASENAME}_fragment"
+  tar cf - -C "$LOCAL_PARENT_DIR" "$LOCAL_RELATIVE_PATH"  | pv -s $(du -sk "$LOCAL_PATH" | awk '{print $1}')k | pigz -4 - | tee >(hash > "${LOCAL_SPLIT_DIR}/${BASENAME}_sha1.txt") | split -b 1024m - "${LOCAL_SPLIT_DIR}/${BASENAME}_fragment"
 
   #delete tar gz file
   rm -rf "$LOCAL_COMPRESSED_FILE_PATH"
@@ -162,9 +169,8 @@ function decompress() {
   # use wildcard search to enable backwards compatibility with older versions
   ORIG_HASH_PATH="$(ls "$TMP_DOWNLOAD_PATH/"*"_sha1.txt")"
   #alias this because they have different names on linux and mac
-  alias shasum=sha1sum
   # pv "${TARGZ_FULLPATH}" | shasum > "$RECON_HASH_PATH"
-  shasum "${TARGZ_FULLPATH}" > "$RECON_HASH_PATH"
+  hash "${TARGZ_FULLPATH}" > "$RECON_HASH_PATH"
   #check if hashed are equal, if so clean up
   if cmp -n 40 -s "$RECON_HASH_PATH" "$ORIG_HASH_PATH" ; then
     echo "SHAs match, cleaning up..."
